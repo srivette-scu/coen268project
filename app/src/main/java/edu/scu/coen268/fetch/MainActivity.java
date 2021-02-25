3a81c22cc33fa01eef586a361fa09f6159c38d35
Spencer Rivette <gaiusjuliuscesar@gmail.com>
2021-02-25 14:57:57 -0800

    splash not working on timer

--- app/src/main/java/edu/scu/coen268/fetch/MainActivity.java
+++ app/src/main/java/edu/scu/coen268/fetch/MainActivity.java
@@ -4,15 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    private boolean showMain = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+        // Launch with a splash page
+        setTheme(R.style.Theme_fetch);
+        setContentView(R.layout.splash_page);
+
+        Timer timer = new Timer();
+        MyTimerTask myTimerTask = new MyTimerTask();
+        timer.schedule(myTimerTask, 3000);
+
+        while(!showMain) {
+            Log.i("TAG", "not set");
+        }
+        Log.i("TAG", "set");
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void showMain() {
+        showMain = true;
     }
 
     public void navigateToLinear(View view) {
@@ -43,5 +65,12 @@ public class MainActivity extends AppCompatActivity {
     public void navigateToConstraint(View view) {
         Intent intent = new Intent(this, ConstraintExample.class);
         startActivity(intent);
+    }
+
+    public class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            showMain();
+        }
     }
 }
---
+++ app/src/main/java/edu/scu/coen268/fetch/SplashActivity.java
@@ -0,0 +1,14 @@
+package edu.scu.coen268.fetch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
+
+}
