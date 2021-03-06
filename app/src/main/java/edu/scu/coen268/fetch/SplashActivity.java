1057b44b44b8df53f1fed4c26b3fd5e4c524bf9d
Spencer Rivette <gaiusjuliuscesar@gmail.com>
2021-03-06 15:01:26 -0800

    Add service and logging

--- app/src/main/java/edu/scu/coen268/fetch/SplashActivity.java
+++ app/src/main/java/edu/scu/coen268/fetch/SplashActivity.java
@@ -1,12 +1,17 @@
 package edu.scu.coen268.fetch;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    String TAG = this.getClass().getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
     }
--- app/src/main/java/edu/scu/coen268/fetch/TermsAndConditionsActivity.java
+++ app/src/main/java/edu/scu/coen268/fetch/TermsAndConditionsActivity.java
@@ -1,12 +1,17 @@
 package edu.scu.coen268.fetch;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TermsAndConditionsActivity extends AppCompatActivity {
+    String TAG = this.getClass().getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.terms_and_conditions_layout);
     }
